68b509ea82f4446593c79688e53f4a416f941cfd
Chris Hatton <mail@ckhatton.com>
2017-01-07 11:39:42 +0000

    Add script to adjust portfolio items at different window widths

--- Source/scripts/script.js
+++ Source/scripts/script.js
@@ -2,9 +2,19 @@
 
 $(document)
   .ready(function() {
+    if ($(window).width() > 750) {
+      $('#portfolio-items .item').removeClass('one-half');
+    } else {
+      $('#portfolio-items .item').addClass('one-half');
+    }
     $('.item-image').height($('.item-image').width());
   })
 $(window)
   .resize(function() {
+    if ($(window).width() > 750) {
+      $('#portfolio-items .item').removeClass('one-half');
+    } else {
+      $('#portfolio-items .item').addClass('one-half');
+    }
     $('.item-image').height($('.item-image').width());
   });
