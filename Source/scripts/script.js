fa08fb1230f0fdea53324f31ffdab016d9ef4d9e
Chris Hatton <mail@ckhatton.com>
2017-01-25 09:26:28 +0000

    Add fancyBox v3 with an alteration that displays a title on the lightbox and resizes the video to avoid showing black banners

--- Source/scripts/script.js
+++ Source/scripts/script.js
@@ -20,6 +20,23 @@ $(document)
   
     $('body').bind('touchstart', function() {});
   
+    $('.fancybox').fancybox({
+      vimeo : {
+        color : 'f00'
+      },
+      afterLoad: function () {
+        if (this.opts.caption) {
+          // Grab the title
+          var newCaption = '<h2>' + this.opts.$orig["0"].dataset.title + '</h2>' + this.opts.caption;
+          // Add the caption beyond the title
+          this.opts.caption = newCaption;
+          console.log('fancyBox caption: ' + this.opts.caption);
+        }
+        
+        $('.fancybox-iframe').height(($('.fancybox-iframe').width()/6)*3);
+      }
+    });
+  
     console.log('%cConstructed by Christopher Hatton (http://about.me/ckhatton).','font-size:12px;color:#13608B;');
 
   });
@@ -34,5 +51,7 @@ $(window)
     }
 
     $('.item-image').height($('.item-image').width());
+  
+    $('.fancybox-iframe').height(($('.fancybox-iframe').width()/6)*3);
 
   });
